766985ac181e27bf49210585f53248c0e6cd5d7b
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-07 14:14:36 -0500

    feat: create an h1 tag with text, and create a line of php that echos text and displays it in the browser.

--- LearnPHP/welcome.php
+++ LearnPHP/welcome.php
@@ -4,9 +4,10 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Welcome to PHP</title>
 </head>
 <body>
-  <?php echo "hello php" ?>
+<h1>This is an h1 tag in HTML</h1>
+  <?php echo "this line is made with php echo" ?>
 </body>
 </html>

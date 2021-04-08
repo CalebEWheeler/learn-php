cc53e9826e894701e920cee139c98055ca7ec8a6
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-08 11:56:55 -0500

    feat: create a for loop that will echo to the browser the value of the counter if the value of counter is less than 10.

---
+++ LearnPHP/ControlStructures/for_loop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>For Loop</title>
+</head>
+<body>
+  <?php 
+    for($counter = 0; $counter < 10; $counter++) {
+      echo $counter." is less than 10..."."<br>";
+    }
+  ?>
+</body>
+</html>

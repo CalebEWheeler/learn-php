937fa7baabe4e36f8539e2a5cee409964b9c1338
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-09 13:58:46 -0500

    feat: create a simple form with some bootstrap for basic styling in preparation for establishing a connection to a MySQL database.

---
+++ LearnPHP/MySQL/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body>
+  <?php ?>
+  <div class="container">
+      <div class="col-xs-6">
+        <form action="login.php" method="POST">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="text" name="password" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="submit" class="btn btn-success" name="submit" value="submit">
+          </div>
+        </form>
+      </div>
+    </div>
+</body>
+</html>

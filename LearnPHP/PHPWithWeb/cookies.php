8e8e281c315550558429f69ee2ad59d5112f6300
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-14 11:32:39 -0500

    feat: use PHP to display the value of the set cookie to the browser.

--- LearnPHP/PHPWithWeb/cookies.php
+++ LearnPHP/PHPWithWeb/cookies.php
@@ -5,8 +5,21 @@ $value = 27;
 $expiration = time() + (60*60*24*7);
 
 setcookie($name, $value, $expiration);
+//essentially I can create conditionals to do things if the cookie is set
 ?>
 
 <?php include "partials/header.php"; ?>
+
+  <?php 
+    if(isset($_COOKIE["Caleb"])) {
+
+      $username = $_COOKIE["Caleb"];
+      //will echo the value of the cookie
+      echo $username;
+
+    } else {
+      $username = "";
+    }
+  ?>
 
 <?php include "partials/footer.php"; ?>

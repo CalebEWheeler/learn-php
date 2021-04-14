419a90b0d82b73dac8130d53d55d731c2a7c14b4
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-14 14:17:05 -0500

    feat: create a method for a car object and learn how to instantiate and call an object method in PHP.

--- LearnPHP/OOP/class_car.php
+++ LearnPHP/OOP/class_car.php
@@ -1,12 +1,32 @@
 <?php 
 
-  class Cakr {
+  class Car {
 
+    function MoveWheels() {
+      echo "Wheels move...";
+    }
   }
 
-  if(class_exists("Car")) {
-    echo "The Car object exists!";
-  } else {
-    echo "no...";
-  }
+  // if(class_exists("Car")) {
+  //   echo "The Car object exists!";
+  // } else {
+  //   echo "no...";
+  // }
+
+  // echo "<br>";
+
+  // if(method_exists("Car", "MoveWheels")) {
+  //   echo "MoveWheels() exists.";
+  // } else {
+  //   echo "The method doesn't exist...";
+  // }
+
+  //Instantiate two new car objects
+  $golf = new Car(); 
+  $jetta = new Car();
+
+  //how to call an objects methods in PHP
+  $golf->MoveWheels();
+  echo "<br>";
+  $jetta->MoveWheels();
 ?>
